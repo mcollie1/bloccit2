6110d74f11af8a4a26890069f815ede3727814c8
mcollie1 <marcus.collier7@yahoo.com>
2014-03-25 13:34:37 -0400

    Implemented favoriting

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Bloccit::Application.routes.draw do
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
-      match '/up-vote', to: 'votes#up_vote', via: :get, as: :up_vote
-      match '/down-vote', to: 'votes#down_vote', via: :get, as: :down_vote
+      get '/up-vote' => 'votes#up_vote', as: :up_vote
+      get '/down-vote' => 'votes#down_vote', as: :down_vote
+      resources :favorites, only: [:create, :destroy]
     end
   end
 
