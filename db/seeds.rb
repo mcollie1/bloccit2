4dc1b5d01675244004c09720a6a1b699b280c543
Marcus Collier <marcus.collier7@yahoo.com>
2015-07-05 21:58:39 -0400

    modified text to represent new purpose of the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,7 +56,7 @@ admin = User.new(
   password_confirmation: 'helloworld')
 admin.skip_confirmation!
 admin.save
-admin.update_attribute(:role, 'admin')
+admin.update_attribute(role: 'admin')
 
 # Create a moderator
 moderator = User.new(
@@ -66,7 +66,7 @@ moderator = User.new(
   password_confirmation: 'helloworld')
 moderator.skip_confirmation!
 moderator.save
-moderator.update_attribute(:role, 'moderator')
+moderator.update_attribute(role: 'moderator')
 
 # Create a member
 member = User.new(
